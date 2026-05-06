99bde70457e250bc09dc8c38fc1f52fe0e103ec1
Byakoren <thomas.gouez-hoarau@live.fr>
2026-05-06 10:48:47 +0200

    Résolution partie 2

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import exo.Partie1;
+import exo.Partie2;
 import factory.TripFactory;
 import models.Trip;
 
@@ -6,9 +7,16 @@ import java.util.List;
 
 void main() {
     List<Trip> trips = TripFactory.generateTrips(10);
-    Partie1 partie1 = new Partie1();
 
-    System.out.println(partie1.longAndExpensiveTrips(trips));
-    System.out.println(partie1.badTrips(trips));
-    System.out.println(partie1.recentTrips(trips));
+    // Partie 1
+    // Partie1 partie1 = new Partie1();
+    // System.out.println(partie1.longAndExpensiveTrips(trips));
+    // System.out.println(partie1.badTrips(trips));
+    // System.out.println(partie1.recentTrips(trips));
+
+    // Partie 2
+    Partie2 partie2 = new Partie2();
+    System.out.println(partie2.countByCity(trips));
+    System.out.println(partie2.revenueByDriver(trips));
+    System.out.println(partie2.avgDurationByCity(trips));
 }
